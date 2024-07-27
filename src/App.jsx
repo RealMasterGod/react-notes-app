140a88950dda54cf74fa1021f26e8e3038c0c207
Master God <gurrakhakhatri@gmail.com>
2024-07-27 13:47:02 +0530

    deployment part2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/react-notes-app/">
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/newNote" element={<NewNote />}/>
